133dcee7087b5ddcabee1d83395880451ca9783e
itznokx <dimitri.b.medeiros@gmail.com>
2025-11-14 20:47:49 -0300

    New bot functionality to check commands in messages

--- bot/app/discord_bot.py
+++ bot/app/discord_bot.py
@@ -1,15 +1,19 @@
 import discord
 import os
+import commands
 from dotenv import load_dotenv
 load_dotenv()
 application_token = os.getenv("DISCORD_TOKEN")
-
-class DiscordClient (discord.Client):
-    async def on_ready(self):
-        printf(f'Logged on as {self.user}')
-    async def on_message(self,message):
-        printf(f'Message from {message.author}:{message.content}')
 intents = discord.Intents.default()
 intents.message_content = True
+class DiscordClient (discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}')
+    async def on_message(self,message):
+        print(f'Message from {message.author}:{message.content}')
+        res = commands.check_command(message)
+        if res:
+            await message.channel.send(res)
 bot = DiscordClient(intents=intents)
 bot.run(application_token)
+
